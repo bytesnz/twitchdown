40ffd6770b532ac5422e48ab623f52c057a138bd
Jack Farley <or4e3dsw@bytes.nz>
2018-09-27 07:54:35 +0100

    Add Preact render test

---
+++ tests/preactRender.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+import test from 'ava';
+import twitchdown from '../index';
+import { readFileSync } from 'fs';
+import { h } from 'preact';
+import { resolve } from 'path';
+
+test.beforeEach((t) => {
+  t.context.old = {
+    log: console.log,
+    warn: console.warn,
+    error: console.error,
+    debug: console.debug
+  };
+
+  t.context.logs = [];
+  const newLog = (level) => (...messages) => t.context.logs.push({
+    level,
+    messages
+  });
+
+  console.log = newLog('log');
+  console.warn = newLog('warn');
+  console.error = newLog('error');
+  console.debug = newLog('debug');
+});
+
+test.afterEach((t) => {
+  console.log = t.context.old.log;
+  console.warn = t.context.old.warn;
+  console.error = t.context.old.error;
+  console.debug = t.context.old.debug;
+});
+
+test('that it renders using React without any errors', (t) => {
+  const markdown = readFileSync(resolve(__dirname, '../README.md')).toString();
+
+  h('p', {}, twitchdown(markdown, {
+    createElement: h
+  }));
+
+  if (t.context.logs.length) {
+    t.log(t.context.logs);
+  }
+  t.deepEqual([], t.context.logs, 'No logs created');
+});
+
