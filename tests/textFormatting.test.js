edb1585714966f50c927e9b1c785deb92e1896ab
Jack Farley <or4e3dsw@bytes.nz>
2020-06-14 19:03:08 +0100

    Add tests for spaces after non-block and block

--- tests/textFormatting.test.js
+++ tests/textFormatting.test.js
@@ -16,3 +16,19 @@ test('parses italics with *', (t) => {
 test('parses italics with _', (t) => {
   t.deepEqual([ 'I ', { type: 'em', props: { key: 1 }, children: [ 'like' ] }, ' tiny libraries' ], twitchdown('I _like_ tiny libraries'));
 });
+
+test('parses bold with ** and a block before', (t) => {
+  t.deepEqual([ { type: 'h1', props: { key: 0 }, children: [ 'Header' ] }, 'I ', { type: 'strong', props: { key: 2 }, children: [ 'like' ] }, ' tiny libraries' ], twitchdown('# Header\nI **like** tiny libraries'));
+});
+
+test('parses bold with __ and a block before', (t) => {
+  t.deepEqual([ { type: 'h1', props: { key: 0 }, children: [ 'Header' ] }, 'I ', { type: 'strong', props: { key: 2 }, children: [ 'like' ] }, ' tiny libraries' ], twitchdown('# Header\nI __like__ tiny libraries'));
+});
+
+test('parses italics with * and a block before', (t) => {
+  t.deepEqual([ { type: 'h1', props: { key: 0 }, children: [ 'Header' ] }, 'I ', { type: 'em', props: { key: 2 }, children: [ 'like' ] }, ' tiny libraries' ], twitchdown('# Header\nI *like* tiny libraries'));
+});
+
+test('parses italics with _ and a block before', (t) => {
+  t.deepEqual([ { type: 'h1', props: { key: 0 }, children: [ 'Header' ] }, 'I ', { type: 'em', props: { key: 2 }, children: [ 'like' ] }, ' tiny libraries' ], twitchdown('# Header\nI _like_ tiny libraries'));
+});
