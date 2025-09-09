a285b0324f46ea5bdebe642dafb2298319982422
Jack Farley <or4e3dsw@bytes.nz>
2025-09-10 09:08:04 +1200

    chore: add tests for description lists

---
+++ tests/descriptionLists.test.js
@@ -0,0 +1,61 @@
+const test = require('ava');
+const twitchdown = require('../index');
+
+test('parses description lists correctly', (t) => {
+  t.deepEqual([
+    'Something with',
+    { type: 'br', props: { key: 0 } },
+    { type: 'dl', props: { key: 1 }, children: [
+      { type: 'dt', props: { key: 2 }, children: [ 'description term 1' ] },
+      { type: 'dd', props: { key: 3 }, children: [ 'description definition 1' ] },
+      { type: 'dt', props: { key: 4 }, children: [ 'description term 2' ] },
+      { type: 'dt', props: { key: 5 }, children: [ 'description term 2a' ] },
+      { type: 'dd', props: { key: 6 }, children: [ 'description definition 2' ] },
+      { type: 'dt', props: { key: 7 }, children: [ 'description term 3' ] },
+      { type: 'dd', props: { key: 8 }, children: [ 'description definition 3' ] },
+      { type: 'dd', props: { key: 9 }, children: [ 'description definition 3a' ] },
+    ] },
+    { type: 'br', props: { key: 10 } },
+    'a line break'
+  ], twitchdown(`Something with
+
+description term 1
+: description definition 1
+description term 2
+description term 2a
+: description definition 2
+description term 3
+: description definition 3
+: description definition 3a
+
+a line break`));
+});
+
+test('parses description lists correctly with paragraphs', (t) => {
+
+  t.deepEqual([
+      { type: 'p', props: { key: 0 }, children: [ 'Something with' ] },
+      { type: 'dl', props: { key: 1 }, children: [
+        { type: 'dt', props: { key: 2 }, children: [ 'description term 1' ] },
+        { type: 'dd', props: { key: 3 }, children: [ 'description definition 1' ] },
+        { type: 'dt', props: { key: 4 }, children: [ 'description term 2' ] },
+        { type: 'dt', props: { key: 5 }, children: [ 'description term 2a' ] },
+        { type: 'dd', props: { key: 6 }, children: [ 'description definition 2' ] },
+        { type: 'dt', props: { key: 7 }, children: [ 'description term 3' ] },
+        { type: 'dd', props: { key: 8 }, children: [ 'description definition 3' ] },
+        { type: 'dd', props: { key: 9 }, children: [ 'description definition 3a' ] },
+      ] },
+      { type: 'p', props: { key: 10 }, children: [ 'a line break' ] },
+    ], twitchdown(`Something with
+
+description term 1
+: description definition 1
+description term 2
+description term 2a
+: description definition 2
+description term 3
+: description definition 3
+: description definition 3a
+
+a line break`, { paragraphs: true }));
+});
